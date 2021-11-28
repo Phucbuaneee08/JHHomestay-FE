33fd12ab17232a69a7c7bc2ee16452f989d4fd40
trandat9h <trantuandat26@gmail.com>
2021-11-28 21:42:44 +0700

    api integrated

--- src/components/homepage/RecommendHotelWrapper.js
+++ src/components/homepage/RecommendHotelWrapper.js
@@ -1,23 +1,28 @@
-import {useEffect} from 'react'
-
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import RecommendedHotelCard from "./RecommendedHotelCard"
-
-const seedHomestay = [
-    {imageURL: "", homestayName: "homestay 1", rating: 5, price:100000, destination:"helloworld" },
-    {imageURL: "", homestayName: "homestay2", rating: 4.6, price:100000, destination:"helloworld" },
-    {imageURL: "", homestayName: "homestay3", rating: 5, price:100000, destination:"helloworld" },
-    {imageURL: "", homestayName: "homestay4", rating: 5, price:100000, destination:"helloworld" },
-]
 
 
 function RecommendedHotelWrapper () {
+    const [data, setData] = useState([])
     useEffect(() => {
+        const fetchData = async() => {
+            try {
+                const {data: response} = await axios.get('http://localhost:8000/homestays/ranking?quantity=4');
+                setData(response.content)
+                console.log(response.content)
+            } 
+            catch (error) {
+                console.error(error.message);
+              }
+        }
+        fetchData()
 
 
     }, [])
     return (
         <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8"> 
-            {seedHomestay.map(homestay => <RecommendedHotelCard detail={homestay} />)}
+            {data.map(homestay => <RecommendedHotelCard detail={homestay} />)}
         </div>
     )
 
--- src/components/homepage/RecommendedHotelCard.js
+++ src/components/homepage/RecommendedHotelCard.js
@@ -5,12 +5,12 @@ const evaluated = [
     {text:"Acceptable", color: "text-yellow-600", bg: "bg-yellow-100"},
 ]
 function RecommendedHotelCard (props) {
-    const {rating, destination, homestayName, price} = props.detail
-    const evaluatedPoint = pointToComment(rating)
+    const {averageRates, address, name, price} = props.detail
+    const evaluatedPoint = pointToComment(averageRates)
     const ratingShow = (
         <div className="flex">
             <div className={`${evaluated[evaluatedPoint].bg} w-10 items-center  text-center rounded-md`}>
-                <p className={`font-bold ${evaluated[evaluatedPoint].color}` }>{rating}</p>
+                <p className={`font-bold ${evaluated[evaluatedPoint].color}` }>{averageRates}</p>
             </div>
             <p className={`ml-2 font-bold ${evaluated[evaluatedPoint].color}` }>{evaluated[evaluatedPoint].text}</p>
 
@@ -19,8 +19,8 @@ function RecommendedHotelCard (props) {
     return (
         <div className="shadow-2xl p-4 rounded-2xl"> 
          <div className="bg-gray-800 h-64 shadow-xl rounded-2xl"></div>
-         <p className="my-2 font-bold text-xl"> {homestayName}</p>
-         <p className="my-2">{destination}</p>
+         <p className="my-2 font-bold text-xl h-12"> {name}</p>
+         <p className="my-2">{address}</p>
          <div className="flex justify-between mt-8">
             {ratingShow}
             <div className="flex flex-row">
