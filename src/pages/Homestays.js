96f1d10d5fb65ac0f9a13b581df67af6470befaf
Nguyễn Quỳnh Anh <quynhanh130701@gmail.com>
2021-12-05 09:02:39 +0700

    QA: fix lại vài chỗ code

--- src/pages/Homestays.js
+++ src/pages/Homestays.js
@@ -4,30 +4,30 @@ import React, { useState} from 'react';
 
 const listHomestays=[
     [
-        {"name":"Homestay 1", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 2", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 3", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 4", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 5", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 6", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 7", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 8", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 9", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 10", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 11", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 12", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 13", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 14", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 15", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 16", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"}
+        {"name":"Homestay 1", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 2", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 3", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 4", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 5", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 6", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 7", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 8", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 9", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 10", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 11", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 12", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 13", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 14", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 15", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 16", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"}
     ],
     [
-        {"name":"Homestay 17", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 18", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 19", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 20", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 21", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"},
-        {"name":"Homestay 22", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "rate":5.0, "href":"#"}
+        {"name":"Homestay 17", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 18", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 19", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 20", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 21", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"},
+        {"name":"Homestay 22", "img":"https://i.pinimg.com/564x/aa/8d/63/aa8d63b5ca82a8d83360ec4072e2412e.jpg", "price":1000000, "type":"1 Phòng ngủ", "averageRates":5.0, "href":"#"}
     ]
 ]
 
